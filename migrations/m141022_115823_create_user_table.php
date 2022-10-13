a0f3814d02933069ea87b169656e8c676e381349
ncsing07 <ncsing@bamako888.com>
2022-10-13 11:48:30 +0800

    Update migration.

--- migrations/m141022_115823_create_user_table.php
+++ migrations/m141022_115823_create_user_table.php
@@ -26,6 +26,17 @@ class m141022_115823_create_user_table extends Migration
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
         ], $tableOptions);
+
+        $this->insert('{{%user}}', [
+            'id' => 1,
+            'username' => 'autoAPI',
+            'email' => 'autoAPI@test.com',
+            'password_hash' => '$2y$13$xXy0IcQPYMYWO9AfhCs9Yebyv8eWxguMyJsN/o82Tx6XPnt5JljY.',
+            'status' => 10,
+            'auth_key' => 'njjdrtUWd-hD-Ayb5o5Lz5GP8T5aWSzC',        
+            'created_at' => 1665629749,
+            'updated_at' => 1665629749,
+        ]);
     }
 
     public function down()
