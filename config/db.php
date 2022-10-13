d92649157a751a319225e3dd6e7ec05e20eb5aae
ncsing07 <ncsing@bamako888.com>
2022-10-13 15:43:24 +0800

    Update docker

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=test_db;port=3306',
+    'dsn' => 'mysql:host=mysql;dbname=test_db;port=3306',
     'username' => 'devuser',
     'password' => 'devpass',
     'charset' => 'utf8',
